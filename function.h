eeae32bdd4b7a2dcb5c391f3f0dacc566ad863a5
duckdog <biker-duck.g@ezweb.ne.jp>
2014-09-18 11:32:54 +0900

    LIEN_hack
    back_up

---
+++ function.h
@@ -0,0 +1,11 @@
+#pragma onece
+
+
+//’n–Ê‚É‚Ì‚Á‚Ä‚é‚©‚ğ‚˜²‚Ì‚İ‚Å”»’è
+bool Rand_on(int man_posx, int rand_posx, int rand_sizex);
+
+//‚©‚×“™‚ÌáŠQ•¨‚Ì‚ ‚½‚è”»’è‚ğ‚˜²‚Æ‚™²‚ğ”ä‚×‚Ä”»’è (’Êí)
+bool Touch(int man_posx, int man_posy, float enemy_posx, float enemy_posy, int enemy_sizex, float enemy_sizey);
+
+//‚©‚×“™‚ÌáŠQ•¨‚Ì‚ ‚½‚è”»’è‚ğ‚˜²‚Æ‚™²‚ğ”ä‚×‚Ä”»’è (BOSSí“¬)
+bool Touch_wall(int man_posx, int man_posy, float enemy_posx, float enemy_posy, int enemy_sizex, float enemy_sizey);
